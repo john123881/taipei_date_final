c4c4f0e995879a82469faf6d248c197349bde140
John <a123881@gmail.com>
2024-03-20 03:36:10 +0800

    finish component Navbar

--- pages/account-center/account-index.js
+++ pages/account-center/account-index.js
@@ -1,41 +1,19 @@
-import React from "react";
-import { FaBookmark } from "react-icons/fa";
-import { MdAccountCircle } from "react-icons/md";
-import { RxBell } from "react-icons/rx";
+import React, { Fragment, useState, useEffect } from "react";
+import styles from "@/styles/ac-index.module.css";
+import Link from "next/link";
+import Navbar from "@/components/account-center";
+
 
 export default function AccountIndex() {
+
     return (
-        <>
-            <div>logo</div>
-            <div className="navbar">
-                <ul>
-                    <li>配對交友</li>
-                    <li>社群媒體</li>
-                    <li>行程規劃</li>
-                    <li>酒吧探索</li>
-                    <li>電影探索</li>
-                </ul>
+        <Fragment>
+            <div className={styles.container} style={{ height: "1500px" }}>
+                <Navbar></Navbar>
+                <div >
+                    <h1>123</h1>
+                </div>
             </div>
-            <div>
-                <ul>
-                    <li>
-                        <a href="#">
-                            <FaBookmark />
-                        </a>
-                    </li>
-                    <li>
-                        <a href="#">
-                            <MdAccountCircle />
-                        </a>
-                    </li>
-                    <li>
-                        {" "}
-                        <a href="#">
-                            <RxBell />
-                        </a>
-                    </li>
-                </ul>
-            </div>
-        </>
+        </Fragment>
     );
 }
